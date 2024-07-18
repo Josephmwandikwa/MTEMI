c7317c64078840d1422fd9071a5c91b38fc26580
Josephmwandikwa <josephmutemi2000@gmail.com>
2024-07-18 16:50:30 +0300

    added

--- MTEMI/MapViewController.swift
+++ MTEMI/MapViewController.swift
@@ -1,210 +1,93 @@
-
 import UIKit
-
 import MapKit
 
-
-
 class MapViewController: UIViewController {
-
     var place: String?
-
     var latitude: Double?
-
     var longitude: Double?
-
     var magnitude: Double?
-
     var earthquake: Earthquake?
 
-
-
     private let mapView: MKMapView = {
-
         let mapView = MKMapView()
-
         mapView.translatesAutoresizingMaskIntoConstraints = false
-
         return mapView
-
     }()
 
-
-
     override func viewDidLoad() {
-
         super.viewDidLoad()
-
         setupUI()
-
         setupNavigation()
-
         setupTapGesture()
 
-
-
         if let earthquake = earthquake {
-
             showEarthquakeLocationOnMap(earthquake)
-
         } else if let latitude = latitude, let longitude = longitude, let place = place, let magnitude = magnitude {
-
             showSelectedLocationOnMap(latitude: latitude, longitude: longitude, place: place, magnitude: magnitude)
-
         }
-
     }
-
-
 
     private func setupUI() {
-
         view.addSubview(mapView)
-
         NSLayoutConstraint.activate([
-
             mapView.topAnchor.constraint(equalTo: view.topAnchor),
-
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-
         ])
-
     }
-
-
 
     private func setupNavigation() {
-
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(backButtonTapped))
-
     }
-
-
 
     @objc private func backButtonTapped() {
-
         navigationController?.popViewController(animated: true)
-
     }
-
-
 
     private func showEarthquakeLocationOnMap(_ earthquake: Earthquake) {
-
-        let annotation = MKPointAnnotation()
-
-        annotation.title = "Magnitude: \(earthquake.magnitude)"
-
-        annotation.subtitle = "\(earthquake.place)"
-
-        annotation.coordinate = CLLocationCoordinate2D(latitude: earthquake.coordinates.latitude, longitude: earthquake.coordinates.longitude)
-
-        mapView.addAnnotation(annotation)
-
-
-
-        let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 500000, longitudinalMeters: 500000)
-
-        mapView.setRegion(region, animated: true)
-
-
-
-        // Select the annotation programmatically
-
-        mapView.selectAnnotation(annotation, animated: true)
-
+        addAnnotation(
+            title: "Magnitude: \(earthquake.magnitude)",
+            subtitle: "\(earthquake.place)",
+            coordinate: CLLocationCoordinate2D(latitude: earthquake.coordinates.latitude, longitude: earthquake.coordinates.longitude)
+        )
     }
-
-
 
     private func showSelectedLocationOnMap(latitude: Double, longitude: Double, place: String, magnitude: Double) {
+        addAnnotation(
+            title: "Magnitude: \(magnitude)",
+            subtitle: "Place: \(place)",
+            coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        )
+    }
 
+    private func addAnnotation(title: String, subtitle: String, coordinate: CLLocationCoordinate2D) {
         let annotation = MKPointAnnotation()
-
-        annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-
-        annotation.title = "Magnitude: \(magnitude)"
-
-        annotation.subtitle = "Place: \(place)"
-
+        annotation.title = title
+        annotation.subtitle = subtitle
+        annotation.coordinate = coordinate
         mapView.addAnnotation(annotation)
 
-
-
         let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 500000, longitudinalMeters: 500000)
-
         mapView.setRegion(region, animated: true)
-
-
-
-        // Select the annotation programmatically
-
-        mapView.selectAnnotation(annotation, animated: true)
-
     }
-
-
 
     private func setupTapGesture() {
-
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleMapTap(_:)))
-
         mapView.addGestureRecognizer(tapGesture)
-
     }
-
-
 
     @objc private func handleMapTap(_ gestureRecognizer: UITapGestureRecognizer) {
-
         let location = gestureRecognizer.location(in: mapView)
-
         let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
 
-
-
-        // Remove existing annotations
-
-//        mapView.removeAnnotations(mapView.annotations)
-
-
-
-        // Add new annotation
-
-//        let annotation = MKPointAnnotation()
-//
-//        annotation.coordinate = coordinate
-//
-//        annotation.title = "Selected Location"
-//
-//        annotation.subtitle = "Lat: \(coordinate.latitude), Lon: \(coordinate.longitude)"
-//
-//        mapView.addAnnotation(annotation)
-
-
-
-        // Optionally, you can zoom in to the selected location
+//        addAnnotation(
+//            title: "Selected Location",
+//            subtitle: "Lat: \(coordinate.latitude), Lon: \(coordinate.longitude)",
+//            coordinate: coordinate
+//        )
 
         let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 50000, longitudinalMeters: 50000)
-
         mapView.setRegion(region, animated: true)
-
-
-
-        // Select the annotation programmatically
-
-//        mapView.selectAnnotation(annotation, animated: true)
-
     }
-
 }
-
-
-
-
-
-
--- MTEMI/ViewController.swift
+++ MTEMI/ViewController.swift
@@ -1,224 +1,145 @@
 import UIKit
-
 import RxSwift
-
 import CoreLocation
 
-
-
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
 
     var tableView: UITableView!
-
     var earthquakes: [Earthquake] = []
-
-
-
+    var filteredEarthquakes: [Earthquake] = []
     let disposeBag = DisposeBag()
-
-
+    let searchController = UISearchController(searchResultsController: nil)
 
     override func viewDidLoad() {
-
         super.viewDidLoad()
 
-
-
-        tableView = UITableView(frame: view.bounds, style: .plain)
-
-        tableView.dataSource = self
-
-        tableView.delegate = self
-
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-
-        tableView.separatorStyle = .none  // Remove default separator line
-
-        view.addSubview(tableView)
-
-
-
+        setupTableView()
+        setupSearchController()
         fetchData()
-
     }
 
+    func setupTableView() {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.separatorStyle = .none  // Remove default separator line
+        view.addSubview(tableView)
+    }
 
+    func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        searchController.searchBar.setImage(UIImage(systemName: "magnifyingglass"), for: .search, state: .normal)
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return earthquakes.count
-
+        return isFiltering() ? filteredEarthquakes.count : earthquakes.count
     }
-
-
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
-        
-
         // Remove all subviews from the cell's content view
-
         cell.contentView.subviews.forEach { $0.removeFromSuperview() }
 
-        
-
-        let earthquake = earthquakes[indexPath.row]
-
-        
+        let earthquake = isFiltering() ? filteredEarthquakes[indexPath.row] : earthquakes[indexPath.row]
 
         let containerView = UIView()
-
         containerView.translatesAutoresizingMaskIntoConstraints = false
-
         containerView.layer.cornerRadius = 10
-
         containerView.layer.masksToBounds = true
-
         containerView.isUserInteractionEnabled = true
-
         containerView.backgroundColor = UIColor.systemGray5
 
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(magnitudeLabelTapped(_:)))
-
-        containerView.addGestureRecognizer(tapGesture)
-
-        
-
         let magnitudeLabel = UILabel()
-
         magnitudeLabel.translatesAutoresizingMaskIntoConstraints = false
-
         magnitudeLabel.text = "Magnitude: \(earthquake.magnitude)"
-
         magnitudeLabel.isUserInteractionEnabled = true
-
         magnitudeLabel.tag = indexPath.row
 
-        
-
         let placeLabel = UILabel()
-
         placeLabel.translatesAutoresizingMaskIntoConstraints = false
-
         placeLabel.isUserInteractionEnabled = true
-
         placeLabel.text = "Place: \(earthquake.place)"
 
-        
-
         containerView.addSubview(magnitudeLabel)
-
         containerView.addSubview(placeLabel)
-
         cell.contentView.addSubview(containerView)
 
-        
-
         NSLayoutConstraint.activate([
-
             containerView.topAnchor.constraint(equalTo: cell.contentView.topAnchor, constant: 10),
-
             containerView.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 10),
-
             containerView.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: -10),
-
             containerView.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor, constant: -10),
 
-            
-
             magnitudeLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 10),
-
             magnitudeLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
-
             magnitudeLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10),
 
-            
-
             placeLabel.topAnchor.constraint(equalTo: magnitudeLabel.bottomAnchor, constant: 5),
-
             placeLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
-
             placeLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10),
-
             placeLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -10)
-
         ])
 
-        
-
         return cell
-
     }
 
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedEarthquake = isFiltering() ? filteredEarthquakes[indexPath.row] : earthquakes[indexPath.row]
+        showMap(for: selectedEarthquake)
+    }
 
     @objc func magnitudeLabelTapped(_ sender: UITapGestureRecognizer) {
-
         guard let label = sender.view else { return }
-
         let index = label.tag
-
-        let earthquake = earthquakes[index]
-
+        let earthquake = isFiltering() ? filteredEarthquakes[index] : earthquakes[index]
         showMap(for: earthquake)
-
     }
-
-
 
     func showMap(for earthquake: Earthquake) {
-
         let mapVC = MapViewController()
-
-        mapVC.place = earthquake.place
-
-        mapVC.latitude = earthquake.coordinates.latitude
-
-        mapVC.longitude = earthquake.coordinates.longitude
-
-        mapVC.magnitude = earthquake.magnitude
-
         mapVC.earthquake = earthquake
-
-
-
         navigationController?.pushViewController(mapVC, animated: true)
-
     }
-
-
 
     func fetchData() {
-
         NetworkManager.shared.fetchEarthquakes()
-
             .subscribe(onNext: { [weak self] earthquakes in
-
                 self?.earthquakes = earthquakes
-
+                self?.filteredEarthquakes = earthquakes
                 DispatchQueue.main.async {
-
                     self?.tableView.reloadData()
-
                 }
-
             }, onError: { error in
-
                 print("Failed to fetch earthquake data:", error)
-
                 // Handle error presentation if needed
-
             })
-
             .disposed(by: disposeBag)
-
     }
 
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        filterContentForSearchText(searchBar.text!)
+    }
+
+    func filterContentForSearchText(_ searchText: String) {
+        filteredEarthquakes = earthquakes.filter { (earthquake: Earthquake) -> Bool in
+            return earthquake.place.lowercased().contains(searchText.lowercased())
+        }
+
+        tableView.reloadData()
+    }
+
+    func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
 }
-
-
-
